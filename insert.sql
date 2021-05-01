27049a6d2d3a9533ab29dbbdc4f66c95c1c1cce7
Pierre Forstmann <15964998+pierreforstmann@users.noreply.github.com>
2021-05-01 16:36:53 +0200

    Delete insert.sql

--- insert.sql
+++
@@ -1,6 +0,0 @@
-truncate table pg_rewrite_rule;
-insert into pg_rewrite_rule(pattern, replacement, enabled) values('select 10;','select 11;',true);
-insert into pg_rewrite_rule(pattern, replacement, enabled) values('select 1+1;','select 1+2;',true);
-insert into pg_rewrite_rule(pattern, replacement, enabled) values('select x from t;','select x,y from t;',true);
-insert into pg_rewrite_rule(pattern, replacement, enabled) values('delete from t;','delete from t where 1=0;',true);
-select count(*) from pg_rewrite_rule;
