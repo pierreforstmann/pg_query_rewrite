7e6f2c5dc90d9a66650cdf08a47a03538ed59843
pierreforstmann <pierre.forstmann@free.fr>
2021-05-25 18:21:39 +0200

    moved mytest to test0

---
+++ sql/test0.sql
@@ -0,0 +1,9 @@
+drop extension if exists pg_query_rewrite;
+--
+create extension pg_query_rewrite;
+--
+select pgqr_add_rule('select 10;','select 11;');
+--
+select 10;
+--
+drop extension pg_query_rewrite;
