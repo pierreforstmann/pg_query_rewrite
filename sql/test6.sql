6375a716d803ccea358444c7e4035c64f0f19884
pierreforstmann <pierre.forstmann@free.fr>
2021-06-06 12:00:54 +0200

    test6.sql: added dynamic SQL case in stored functions test

--- sql/test6.sql
+++ sql/test6.sql
@@ -7,27 +7,49 @@ select pgqr_truncate_rule();
 drop table if exists t;
 create table t(i int);
 --
-drop function if exists f;
-create function f(IN p int, OUT i int) returns int as $$
+drop function if exists fs;
+create function fs(IN p int, OUT i int) returns int as $$
 begin
-    insert into t(i) values (f.p) returning t.i into f.i;
+    insert into t(i) values (fs.p) returning t.i into fs.i;
 end$$
 language plpgsql;
 --
-select f(1);
+select fs(1);
 select * from t;
 select pgqr_rules();
 --
-select pgqr_add_rule('insert into t(i) values (f.p) returning t.i', 'insert into t(i) values(2) returning t.i');
+select pgqr_add_rule('insert into t(i) values (fs.p) returning t.i', 'insert into t(i) values(2) returning t.i');
 --
-drop function f;
-create function f(IN p int, OUT i int) returns int as $$
+drop function fs;
+create function fs(IN p int, OUT i int) returns int as $$
 begin
-    insert into t(i) values (f.p) returning t.i into f.i;
+    insert into t(i) values (fs.p) returning t.i into fs.i;
 end$$
 language plpgsql;
 --
-select f(1);
+select fs(1);
 select * from t;
 select pgqr_rules();
 --
+--
+--
+truncate table t;
+select fs(1);
+select * from t;
+--
+drop function if exists fd;
+create function fd(IN p int, OUT i int) returns int as $$
+begin
+    execute 'select count(*) from t where i = ' || p into fd.i using fd.p;
+end$$
+language plpgsql;
+--
+select fd(2);
+select * from t;
+select pgqr_rules();
+--
+select pgqr_add_rule('select count(*) from t where i = 2', 'select count(*) from t where i = 3');
+--
+select fd(2);
+select * from t;
+select pgqr_rules();
