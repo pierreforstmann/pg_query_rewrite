d81d49c20c5b1d1cf98fb1d1e16382a8a4bc0399
pierreforstmann <pierre.forstmann@free.fr>
2021-06-08 18:29:07 +0200

    adapted test6 for PG 9.6.22 (drop function)

--- sql/test6.sql
+++ sql/test6.sql
@@ -7,7 +7,7 @@ select pgqr_truncate_rule();
 drop table if exists t;
 create table t(i int);
 --
-drop function if exists fs;
+drop function if exists fs(in int, out int);
 create function fs(IN p int, OUT i int) returns int as $$
 begin
     insert into t(i) values (fs.p) returning t.i into fs.i;
@@ -20,7 +20,7 @@ select pgqr_rules();
 --
 select pgqr_add_rule('insert into t(i) values (fs.p) returning t.i', 'insert into t(i) values(2) returning t.i');
 --
-drop function fs;
+drop function fs(in int, out int);
 create function fs(IN p int, OUT i int) returns int as $$
 begin
     insert into t(i) values (fs.p) returning t.i into fs.i;
@@ -37,7 +37,7 @@ truncate table t;
 select fs(1);
 select * from t;
 --
-drop function if exists fd;
+drop function if exists fd(in int, out int);
 create function fd(IN p int, OUT i int) returns int as $$
 begin
     execute 'select count(*) from t where i = ' || p into fd.i using fd.p;
